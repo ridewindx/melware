b4914ac8f8bde728ed1b48c580440eeab9151c84
RideWindX <ridewindx@163.com>
2017-01-15 23:57:13 +0800

    Update

--- jwt.go
+++ jwt.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"strings"
 	"time"
+	"github.com/gin-gonic/gin"
 )
 
 // JWT provides a Json-Web-Token authentication implementation.
@@ -41,4 +42,29 @@ type JWT struct {
 	// Must return true on success, false on failure.
 	// Required. Option return user id, if so, user id will be stored in Claim Array.
 	Authenticator func(userID string, password string, c *mel.Context) (string, bool)
+
+	// Callback function that should perform the authorization of the authenticated user. Called
+	// only after an authentication success. Must return true on success, false on failure.
+	// Optional, default to success.
+	Authorizator func(userID string, c *gin.Context) bool
+
+	// Callback function that will be called during login.
+	// Using this function it is possible to add additional payload data to the webtoken.
+	// The data is then made available during requests via c.Get("JWT_PAYLOAD").
+	// Note that the payload is not encrypted.
+	// The attributes mentioned on jwt.io can't be used as keys for the map.
+	// Optional, by default no additional data will be set.
+	PayloadFunc func(userID string) map[string]interface{}
+
+	// User can define own Unauthorized func.
+	Unauthorized func(*gin.Context, int, string)
+
+	// TokenLookup is a string in the form of "<source>:<name>" that is used
+	// to extract token from the request.
+	// Optional. Default value "header:Authorization".
+	// Possible values:
+	// - "header:<name>"
+	// - "query:<name>"
+	// - "cookie:<name>"
+	TokenLookup string
 }
