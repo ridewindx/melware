cac955f6042b24866b93add858f4585200ab01cf
RideWindX <ridewindx@163.com>
2017-01-17 20:49:45 +0800

    Update jwt.go

--- jwt.go
+++ jwt.go
@@ -38,47 +38,81 @@ type JWT struct {
 	// Optional. Defaults to 0 meaning not refreshable.
 	MaxRefresh   time.Duration
 
-	// Authenticator specifies the callback that should perform the authentication
+	// Authenticate specifies the callback that should perform the authentication
 	// of the user based on userID and password.
 	// Must return true on success, false on failure.
 	// Required. Option return user id, if so, user id will be stored in Claim Array.
 	Authenticate func(userID string, password string, c *mel.Context) (string, bool)
 
-	// Authorizator specifies the callback that should perform the authorization
+	// Authorize specifies the callback that should perform the authorization
 	// of the authenticated user.
 	// Must return true on success, false on failure.
 	// Optional. Default to always return success.
-	Authorizator func(userID string, c *gin.Context) bool
+	Authorize    func(userID string, c *mel.Context) bool
 
 	// PayloadFunc specifies the callback that will be called during login.
 	// It is useful for adding additional payload data to the token.
-	// The data is then made available during requests via c.Get("JWT_PAYLOAD").
+	// The data is then made available during requests via ExtractClaims(PayloadKey).
 	// Note that the payload is not encrypted.
 	// Optional. By default no additional payload will be added.
-	PayloadFunc func(userID string) map[string]interface{}
+	PayloadFunc  func(userID string) map[string]interface{}
 
 	// Unauthorized specifies the unauthorized function.
 	Unauthorized func(*gin.Context, int, string)
 
-	// TokenLookup is a string in the form of "<source>:<name>" that is used
-	// to extract token from the request.
-	// Optional. Default value "header:Authorization".
+	// TokenBearer is a string in the form of "<source>:<name>"
+	// that is used to extract token from the request.
+	// Optional. Default to "header:Authorization".
 	// Possible values:
 	// - "header:<name>"
 	// - "query:<name>"
 	// - "cookie:<name>"
-	TokenLookup string
+	TokenBearer  string
+
+	// PayloadKey specifies the key when puts JWT payload into Context.
+	// Optional. Default to "JWT_PAYLOAD".
+	PayloadKey string
 
 	extractToken func(*mel.Context) (string, error)
 }
 
-type Login struct {
-	Username string `form:"username" json:"username" binding:"required"`
-	Password string `form:"password" json:"password" binding:"required"`
-}
-
 func (j *JWT) init() {
-	parts := strings.Split(j.TokenLookup, ":")
+	if len(j.Realm) == 0 {
+		panic("Realm is required")
+	}
+
+	if len(j.SigningAlgorithm) == 0 {
+		j.SigningAlgorithm = "HS256"
+	}
+
+	if j.Key == nil {
+		panic("Secret key is required")
+	}
+
+	if j.Timeout == 0 {
+		j.Timeout = time.Hour
+	}
+
+	if j.Authenticate == nil {
+		panic("Authenticate funciton is required")
+	}
+
+	if j.Authorize == nil {
+		j.Authorize = func(userId string, c *mel.Context) bool {
+			return true
+		}
+	}
+
+	if j.Unauthorized == nil {
+		j.Unauthorized = func(c *mel.Context, code int, message string) {
+			c.JSON(code, mel.Map{
+				"code": code,
+				"message": message,
+			})
+		}
+	}
+
+	parts := strings.Split(j.TokenBearer, ":")
 	key := parts[1]
 	switch parts[0] {
 	case "header":
@@ -96,6 +130,7 @@ func (j *JWT) init() {
 
 			return parts[1], nil
 		}
+
 	case "query":
 		j.extractToken = func(c *mel.Context) (string, error) {
 			token := c.Query(key)
@@ -106,6 +141,7 @@ func (j *JWT) init() {
 
 			return token, nil
 		}
+
 	case "cookie":
 		j.extractToken = func(c *mel.Context) (string, error) {
 			cookie, _ := c.Cookie(key)
@@ -116,10 +152,14 @@ func (j *JWT) init() {
 
 			return cookie, nil
 		}
+
+	default:
+		panic("Invalid token source")
 	}
 
-
-	return nil
+	if len(j.PayloadKey) == 0 {
+		j.PayloadKey = "JWT_PAYLOAD"
+	}
 }
 
 // Middleware returns a middleware that authorizes tokens.
@@ -137,10 +177,10 @@ func (j *JWT) Middleware() mel.Handler {
 		claims := token.Claims.(jwt.MapClaims)
 
 		userId := claims["id"].(string)
-		c.Set("JWT_PAYLOAD", claims)
+		c.Set(j.PayloadKey, claims)
 		c.Set("userID", userId)
 
-		if !j.Authorizator(userId, c) {
+		if !j.Authorize(userId, c) {
 			j.unauthorized(c, http.StatusForbidden, "You don't have permission to access.")
 			return
 		}
@@ -157,6 +197,11 @@ func (j *JWT) LoginHandler() mel.Handler {
 	j.init()
 
 	return func(c *mel.Context) {
+		type Login struct {
+			Username string `form:"username" json:"username" binding:"required"`
+			Password string `form:"password" json:"password" binding:"required"`
+		}
+
 		var login Login
 
 		if c.BindJSON(&login) != nil {
@@ -203,6 +248,51 @@ func (j *JWT) LoginHandler() mel.Handler {
 	}
 }
 
+// RefreshHandler can be used to refresh a token.
+// The token still needs to be valid on refresh.
+// Shall be put under an endpoint that is using the Middleware.
+// Reply will be of the form {"token": "TOKEN"}.
+func (j *JWT) RefreshHandler(c *gin.Context) {
+	token, _ := j.parseToken(c)
+	claims := token.Claims.(jwt.MapClaims)
+
+	iat := int64(claims["iat"].(int64))
+
+	if iat < time.Now().Add(-j.MaxRefresh).Unix() {
+		j.unauthorized(c, http.StatusUnauthorized, "Token is expired")
+		return
+	}
+
+    // Refresh expiration time
+	expire := time.Now().Add(j.Timeout)
+	claims["exp"] = expire.Unix()
+
+	// Create the token
+	alg := jwt.GetSigningMethod(j.SigningAlgorithm)
+	newToken := jwt.NewWithClaims(alg, claims)
+
+	tokenStr, err := newToken.SignedString(j.Key)
+	if err != nil {
+		j.unauthorized(c, http.StatusUnauthorized, "Create JWT Token faild")
+		return
+	}
+
+	c.JSON(http.StatusOK, mel.Map{
+		"token":  tokenStr,
+		"expires_at": expire.Format(time.RFC3339),
+	})
+}
+
+// ExtractClaims extracts the JWT claims.
+func (j *JWT) ExtractClaims(c *mel.Context) jwt.MapClaims {
+	claims, ok := c.Get(j.PayloadKey)
+	if ok {
+		return claims.(jwt.MapClaims)
+	} else {
+		return nil
+	}
+}
+
 func (j *JWT) parseToken(c *mel.Context) (*jwt.Token, error) {
 	tokenStr, err := j.extractToken(c)
 
@@ -220,10 +310,6 @@ func (j *JWT) parseToken(c *mel.Context) (*jwt.Token, error) {
 }
 
 func (mw *JWT) unauthorized(c *mel.Context, code int, message string) {
-	if mw.Realm == "" {
-		mw.Realm = "mel jwt"
-	}
-
 	c.Header("WWW-Authenticate", "JWT realm="+mw.Realm)
 	c.Abort()
 
