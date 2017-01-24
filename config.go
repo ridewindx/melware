0af50728c1ca829ed1dca25301a5a527462cf5e2
RideWindX <ridewindx@163.com>
2017-01-25 00:00:15 +0800

    Update

--- config.go
+++ config.go
@@ -12,8 +12,8 @@ type config struct {
 	viper.Viper
 }
 
-// Config returns a config object for the Mel instance.
-// If the config object doesn't exist in Mel, create a new one
+// Config returns the config for a Mel instance.
+// If the config doesn't exist in Mel, create a new one
 // and put it into Mel.
 // The config object inherently has all the methods of viper.Viper.
 func Config(mel *mel.Mel) *config {
@@ -32,15 +32,18 @@ func Config(mel *mel.Mel) *config {
 	return c
 }
 
+// GetConfig gets the Mel config from mel.Context.
 func GetConfig(c *mel.Context) *config {
 	return Config(c.Mel)
 }
 
+// ReadFile reads config from the specified file.
 func (c *config) ReadFile(configFile string) error {
 	c.SetConfigFile(configFile)
 	return c.ReadInConfig()
 }
 
+// ReadDirs reads config from a found file named as configName in several directories.
 func (c *config) ReadDirs(configName string, dirs ...string) {
 	c.SetConfigName(configName)
 	for _, dir := range dirs {
