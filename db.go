6f1f092c3efda7e1cee470fc21c0c774e3f86aed
RideWindX <ridewindx@163.com>
2017-02-04 10:49:43 +0800

    Update db.go

--- db.go
+++ db.go
@@ -2,6 +2,7 @@ package melware
 
 import (
 	"database/sql"
+    "time"
 	"github.com/spf13/viper"
 	"github.com/ridewindx/mel"
 )
@@ -11,7 +12,7 @@ func DBConfig(mel *mel.Mel) *viper.Viper {
 	dbConfig := config.Sub("db")
 
 	dbConfig.SetDefault("conn_max_lifetime", 0)
-	dbConfig.SetDefault("mac_idle_conns", 0)
+	dbConfig.SetDefault("max_idle_conns", 0)
 	dbConfig.SetDefault("max_open_conns", 0)
 
 	return dbConfig
@@ -23,6 +24,16 @@ func DB(mel *mel.Mel) *sql.DB {
 	if err != nil {
 		panic(err)
 	}
-	db.SetConnMaxLifetime(dbConfig.GetInt64("conn_max_lifetime"))
+	db.SetConnMaxLifetime(time.Duration(dbConfig.GetInt64("conn_max_lifetime")))
+	db.SetMaxIdleConns(dbConfig.GetInt("max_idle_conns"))
+	db.SetMaxOpenConns(dbConfig.GetInt("max_open_conns"))
+
+    // Send a ping to make sure the database connection is alive.
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		panic(err)
+	}
+
 	return db
 }
